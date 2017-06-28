4489d79114b446dbc596ef9c01938bf56257f88d
dqq <1248321610@qq.com>
2017-06-28 14:30:14 +0800

    heheh

---
+++ src/main/java/Maven01/Dqq/App.java
@@ -0,0 +1,13 @@
+package Maven01.Dqq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
